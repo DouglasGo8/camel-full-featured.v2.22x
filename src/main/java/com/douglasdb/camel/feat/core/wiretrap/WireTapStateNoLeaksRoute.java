7df6dbc5cc1eeb09fba9a369beb97ae444b15e11
DouglasGo8 <abraham.dias.douglas@gmail.com>
2019-02-18 21:43:14 -0300

    Adjust1

--- src/main/java/com/douglasdb/camel/feat/core/wiretrap/WireTapStateNoLeaksRoute.java
+++ src/main/java/com/douglasdb/camel/feat/core/wiretrap/WireTapStateNoLeaksRoute.java
@@ -3,8 +3,10 @@ package com.douglasdb.camel.feat.core.wiretrap;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 
-import com.douglasdb.camel.feat.core.domain.Cheese;
-
+import com.douglasdb.camel.feat.core.domain.Cheese;;
+/**
+ * @author Douglas D.b
+ */
 public class WireTapStateNoLeaksRoute extends RouteBuilder {
 
 
@@ -13,21 +15,19 @@ public class WireTapStateNoLeaksRoute extends RouteBuilder {
 
 
         from("direct:start")
-                .log("Cheese is ${body.age} months old")
-                .wireTap("direct:processInBackground")
+            .log("Cheese is ${body.age} months old")
+            .wireTap("direct:processInBackground")
                 .onPrepare(exchange -> {
                     Message in = exchange.getIn();
                     Cheese cheese = in.getBody(Cheese.class);
                     if (cheese != null)
                         in.setBody(cheese.clone());
                 })
-                .delay(1000)
-                .to("mock:out");
-
-
+            .delay(1000) // forces delay
+            .to("mock:out");
 
         from("direct:processInBackground")
-                .bean(CheeseRipener.class, "ripen")
-                .to("mock:tapped");
+            .bean(CheeseRipener.class, "ripen")
+            .to("mock:tapped");
     }
 }
