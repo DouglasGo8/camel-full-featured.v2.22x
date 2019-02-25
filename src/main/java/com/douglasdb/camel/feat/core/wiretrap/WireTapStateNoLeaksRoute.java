c9c7bd3256d5e4a887584db841f930623a5dbf56
DouglasGo8 <abraham.dias.douglas@gmail.com>
2019-02-25 20:26:25 -0300

    Wiretrap

--- src/main/java/com/douglasdb/camel/feat/core/wiretrap/WireTapStateNoLeaksRoute.java
+++ src/main/java/com/douglasdb/camel/feat/core/wiretrap/WireTapStateNoLeaksRoute.java
@@ -23,20 +23,16 @@ public class WireTapStateNoLeaksRoute extends RouteBuilder {
                     if (cheese != null)
                         in.setBody(cheese.clone());
                 })
-<<<<<<< HEAD
             .delay(1000) // forces delay
             .to("mock:out");
-=======
-                .delay(1000)
-                .to("mock:out");
->>>>>>> gc001
+                
 
         from("direct:processInBackground")
-<<<<<<< HEAD
+
          	.bean(CheeseRipener.class, "ripen")
-=======
+
             .bean(CheeseRipener.class, "ripen")
->>>>>>> 7df6dbc5cc1eeb09fba9a369beb97ae444b15e11
+
             .to("mock:tapped");
     }
 }
