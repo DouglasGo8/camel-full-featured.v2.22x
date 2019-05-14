e6d9bf1dad61092668c5883d17978609fc524256
DouglasGo8 <sgtear@hotmail.com>
2019-05-13 22:43:56 -0300

    Update

--- src/main/java/com/douglasdb/camel/feat/core/domain/jaxb/BookModel.java
+++ src/main/java/com/douglasdb/camel/feat/core/domain/jaxb/BookModel.java
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
 
-package com.douglasdb.camel.feat.core.domain.jaxb;
-
-import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-/**
- * 
- */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class BookModel implements Serializable {
-
-
-
-    private String title;
-    private String author1;
-    private String author2;
-
-    private String category;
-    private String titleLanguage;
-
-
-    private BigDecimal price;
-
-    private LocalDate publishDate;
-
-
-=======
 
 package com.douglasdb.camel.feat.core.domain.jaxb;
 
@@ -72,6 +36,4 @@ public class BookModel implements Serializable {
 
     private LocalDate publishDate;
 
-
->>>>>>> a8bd96d7fc224dc22037cb97a3bd5d97a10b6cca
 }
