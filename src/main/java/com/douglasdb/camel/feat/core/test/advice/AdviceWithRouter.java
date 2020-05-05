21101f3b51da53b2abbb727ba066f07c60fd762b
DouglasGo8 <sgtear@hotmail.com>
2020-05-04 22:09:20 -0300

    AdviceWith

---
+++ src/main/java/com/douglasdb/camel/feat/core/test/advice/AdviceWithRouter.java
@@ -0,0 +1,22 @@
+package com.douglasdb.camel.feat.core.test.advice;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class AdviceWithRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+
+
+        from("seda:quotes")
+                .routeId("quotes")
+                .choice()
+                .when(simple("${body} contains 'Camel'"))
+                    .to("seda:camel")
+                .otherwise()
+                    .to("seda:other")
+                .end();
+    }
+
+}
